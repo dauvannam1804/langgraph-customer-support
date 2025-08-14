4ac554ef4985d02fb87740fedd5fcbdd30124ef7
dauvannam1804 <dauvannam321@gmail.com>
2025-08-14 14:55:20 +0700

    refactor: organize source code again

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Đường dẫn và URL của cơ sở dữ liệu
-DB_URL = "https://storage.googleapis.com/benchmarks-artifacts/travel-db/travel2.sqlite"
-LOCAL_DB_FILE = "travel2.sqlite"
-BACKUP_DB_FILE = "travel2.backup.sqlite"
-
-# Đặt True để buộc tải lại và ghi đè cơ sở dữ liệu khi khởi động ứng dụng
-OVERWRITE_DB_ON_STARTUP = False
